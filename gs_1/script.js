362cfd832a4fddd3bfac0ff6dc9eaded27af69f0
albertofanini-prog <albertofanini@outlook.com>
2022-07-04 19:59:24 +0200

    basic effects on images

--- gs_1/script.js
+++ gs_1/script.js
@@ -1,1 +1,29 @@
-console.log('First test Greensock');
+// console.log('First test Greensock');
+
+//gsap.to identica l'elemento
+//duration: durata del movimento
+//x: sposta su asse x, y: sposta su asse y
+//borderRadius: imposta il border radius
+//rotation: ruota l'elemento
+//slcae: scala l'elemento
+//opacity: opacizza l'elemento
+gsap.to('.github',{
+    duration: 2,
+    x: 250,
+    backgroundColor: '#154F30',
+    borderRadius: '9999px',
+    border: '3px solid #154F30',
+    rotation: 360,
+    scale: 1.5
+});
+
+
+gsap.to('.vs',{
+    duration: 2,
+    x: -250,
+    backgroundColor: '#9F4FA3',
+    borderRadius: '9999px',
+    border: '3px solid #9F4FA3',
+    rotation: 360,
+    scale: 1.5
+});
